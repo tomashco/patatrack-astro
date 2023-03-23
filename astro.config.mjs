a7e3469118d306b4b294969a657a20fd5e9b3251
Maioli Tommaso <tmaioli@deloitte.it>
2023-03-23 16:42:18 +0100

    feat: integration of netlify-cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,7 @@ import compress from 'astro-compress';
 import { readingTimeRemarkPlugin } from './src/utils/frontmatter.mjs';
 
 import { SITE } from './src/config.mjs';
+import NetlifyCMS from 'astro-netlify-cms';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -40,7 +41,32 @@ export default defineConfig({
       serviceEntryPoint: '@astrojs/image/sharp',
     }),
     mdx(),
-
+    NetlifyCMS({
+      config: {
+        backend: {
+          name: 'git-gateway',
+          branch: 'main',
+        },
+        collections: [
+          // Content collections
+          {
+            name: 'post',
+            label: 'Blog Posts',
+            folder: 'src/content/post',
+            create: true,
+            delete: true,
+            fields: [
+              { name: 'title', widget: 'string', label: 'Post Title' },
+              { name: 'body', widget: 'markdown', label: 'Post Body' },
+              { name: 'publishDate', widget: 'datetime', label: 'Publish Date' },
+              { name: 'excerpt', widget: 'string', label: 'post Description' },
+              { name: 'image', widget: 'image', label: 'post Image' },
+              { name: 'tags', widget: 'list', default: ['term_1', 'term_2'], label: 'Tags' },
+            ],
+          },
+        ],
+      },
+    }),
     ...whenExternalScripts(() =>
       partytown({
         config: { forward: ['dataLayer.push'] },
