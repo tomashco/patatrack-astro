9d5fce6e34f0420a3a341eccfc6a0f5b2e16e6b3
Maioli Tommaso <tmaioli@deloitte.it>
2023-04-11 21:00:09 +0200

    feat: addition of Mantine Carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,75 +1,59 @@
 import { AdvancedImage } from '@cloudinary/react';
 import cld from '~/cloudinaryConfig';
+import { MantineProvider } from '@mantine/core';
+import { Carousel } from '@mantine/carousel';
 import { useState } from 'react';
 import useKeypress from '~/hooks/useKeypress';
-import { fill } from '@cloudinary/url-gen/actions/resize';
 
 interface CarouselProps {
   gallery: Array<string>;
 }
 
-type imgState = {
-  image: string;
-  index: number;
-};
-const ShowFullImage = ({ image }) => {
+const ShowFullImage = ({ children }) => {
   return (
-    // <div className="fixed z-[999] top-0 left-0 bg-black bg-opacity-80 flex justify-center align-middle">
-    <div className="fixed z-[999] w-full h-full top-0 left-0 bg-black bg-opacity-80 flex justify-center align-middle">
-      <div className="fixed flex portrait:w-full landscape:h-full justify-center">
-        <AdvancedImage cldImg={cld.image(image)} />
-      </div>
+    <div className="fixed z-[998] w-full h-full top-0 left-0 bg-black bg-opacity-80 flex justify-center align-middle">
+      <div className="fixed flex w-full h-full items-center justify-center">{children}</div>
     </div>
   );
 };
 
-export default function Carousel({ gallery }: CarouselProps) {
-  const resetImage = { image: '', index: 0 };
-  const [imageSelected, setImageSelected] = useState(resetImage);
+const CarouselComponent = ({ gallery, action, isFullScreen = false }) => (
+  <Carousel
+    onClick={() => action((value) => !value)}
+    maw={'100%'}
+    mx="auto"
+    withIndicators
+    height={isFullScreen ? '100%' : '400'}
+  >
+    {gallery.map((slide) => (
+      <Carousel.Slide key={slide}>
+        <AdvancedImage cldImg={cld.image(slide)} />
+      </Carousel.Slide>
+    ))}
+  </Carousel>
+);
+
+export default function CarouselContainer({ gallery }: CarouselProps) {
+  const [isFullScreen, toggleFullScreen] = useState(false);
 
   useKeypress('Escape', () => {
-    setImageSelected(resetImage);
+    toggleFullScreen(false);
   });
 
-  useKeypress(
-    'ArrowRight',
-    ({ image }: imgState) => {
-      if (image)
-        setImageSelected((imageSel: imgState) => {
-          const newInd = Math.min(gallery.length - 1, imageSel.index + 1);
-          return { image: gallery[newInd], index: newInd };
-        });
-    },
-    imageSelected
-  );
-
-  useKeypress(
-    'ArrowLeft',
-    ({ image }: imgState) => {
-      if (image)
-        setImageSelected((imageSel: imgState) => {
-          const newInd = Math.max(0, imageSel.index - 1);
-          return { image: gallery[newInd], index: newInd };
-        });
-    },
-    imageSelected
-  );
-
   return (
-    <>
-      <div className="grid gap-6 row-gap-5 md:grid-cols-2 lg:grid-cols-4 -mb-6">
-        {gallery.map((slide, index) => (
-          <div
-            key={slide}
-            onClick={() => {
-              setImageSelected({ image: slide, index: index });
-            }}
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <CarouselComponent gallery={gallery} action={toggleFullScreen} />
+      {isFullScreen && (
+        <ShowFullImage>
+          <strong
+            onClick={() => toggleFullScreen(false)}
+            className="drop-shadow-2xl fixed z-[998] w-9 h-9 flex justify-center items-center rounded-lg bg-opacity-80 bg-slate-400 top-0 right-0 font-black text-violet-400 text-3xl cursor-pointer alert-del"
           >
-            <AdvancedImage cldImg={cld.image(slide)} />
-          </div>
-        ))}
-      </div>
-      {imageSelected.image && <ShowFullImage image={imageSelected.image} />}
-    </>
+            &times;
+          </strong>
+          <CarouselComponent gallery={gallery} action={() => {}} isFullScreen />
+        </ShowFullImage>
+      )}
+    </MantineProvider>
   );
 }
