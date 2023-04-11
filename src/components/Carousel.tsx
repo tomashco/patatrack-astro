64a8f78c9ab419437ac3bc163551d58dbeb8381c
Maioli Tommaso <tmaioli@deloitte.it>
2023-04-11 08:46:06 +0200

    feat: initial integration of carousel react component

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,74 @@
 import { AdvancedImage } from '@cloudinary/react';
 import cld from '~/cloudinaryConfig';
+import { useState } from 'react';
+import useKeypress from '~/hooks/useKeypress';
+import { AspectRatio } from '@cloudinary/url-gen/qualifiers';
 
 interface CarouselProps {
   gallery: Array<string>;
 }
 
-export default function Carousel({ gallery }: CarouselProps) {
+type imgState = {
+  image: string;
+  index: number;
+};
+const ShowFullImage = ({ image }) => {
   return (
-    <div className="grid gap-6 row-gap-5 md:grid-cols-2 lg:grid-cols-4 -mb-6">
-      {gallery.map((slide) => (
-        <div key={slide} onClick={() => alert(slide)}>
-          <AdvancedImage cldImg={cld.image(slide)} />
-        </div>
-      ))}
+    <div className="fixed z-[999] w-full h-full top-0 left-0 bg-black bg-opacity-80 flex justify-center align-middle">
+      <div className="w-auto flex align-middle justify-center">
+        <AdvancedImage cldImg={cld.image(image)} />
+      </div>
     </div>
+  );
+};
+
+export default function Carousel({ gallery }: CarouselProps) {
+  const resetImage = { image: '', index: 0 };
+  const [imageSelected, setImageSelected] = useState(resetImage);
+
+  useKeypress('Escape', () => {
+    setImageSelected(resetImage);
+  });
+
+  useKeypress(
+    'ArrowRight',
+    ({ image }: imgState) => {
+      if (image)
+        setImageSelected((imageSel: imgState) => {
+          const newInd = Math.min(gallery.length - 1, imageSel.index + 1);
+          return { image: gallery[newInd], index: newInd };
+        });
+    },
+    imageSelected
+  );
+
+  useKeypress(
+    'ArrowLeft',
+    ({ image }: imgState) => {
+      if (image)
+        setImageSelected((imageSel: imgState) => {
+          const newInd = Math.max(0, imageSel.index - 1);
+          return { image: gallery[newInd], index: newInd };
+        });
+    },
+    imageSelected
+  );
+
+  return (
+    <>
+      <div className="grid gap-6 row-gap-5 md:grid-cols-2 lg:grid-cols-4 -mb-6">
+        {gallery.map((slide, index) => (
+          <div
+            key={slide}
+            onClick={() => {
+              setImageSelected({ image: slide, index: index });
+            }}
+          >
+            <AdvancedImage cldImg={cld.image(slide)} />
+          </div>
+        ))}
+      </div>
+      {imageSelected.image && <ShowFullImage image={imageSelected.image} />}
+    </>
   );
 }
